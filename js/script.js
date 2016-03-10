23c4ab54d3d359c2348fac027ffc8d6584767791
Nicoal Price <nicoalprice@gmail.com>
2016-03-10 17:39:14 -0500

    further setup of files and folders

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/* Create a lightbox for viewing full size images when their thumbnails are clicked. */
+
+/* Add text captions to the images when viewed in the lightbox. */
+
+/* Add back and forward buttons when the lightbox is visible to switch between photos. */
+
+/* Implement the search box at the top of the page that filters photos based on the captions.
+    The photos should filter in real-time as you type.
+    This could be a jQuery plugin that you find on the web, or code that you write yourself. */
+
+/* */
+
