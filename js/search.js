ac4f3f969de34f2f9af19e913137b07324f55d37
Nicoal Price <nicoalprice@gmail.com>
2016-03-18 12:16:50 -0400

    fixed js search

--- js/search.js
+++ js/search.js
@@ -1,17 +1,24 @@
 /* after key is pressed */
 $("#search").keyup(function() {
+
     /* store searched value */
     var $search = $(this).val();
+
     /* loop over each image */
     $("#gallery img").each(function() {
+
         /* get alt text */
         var $altText = $(this).attr("alt");
+
         /* if searched value matches alt keywords, show image */
         if($altText.toLowerCase().search($search.toLowerCase()) > -1) {
+            /* show matching images */
             $(this).show();
-        /* hide image if no matches are found */
+            /* move matches to beginning of gallery but retain spacing */
+
+            /* hide image if no matches are found */
         } else {
-            $(this).fadeOut();
+            $(this).fadeOut("500");
         }
     });
 });
