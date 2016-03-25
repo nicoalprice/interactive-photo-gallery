f0c3c5e1c4be949bda6429e4a59155c1c0c4fecc
Nicoal Price <nicoalprice@gmail.com>
2016-03-24 20:45:12 -0400

    perfecting lightbox navigation so it loops through gallery

--- js/lightbox.js
+++ js/lightbox.js
@@ -83,7 +83,7 @@ $exit.on("click", function() {
 /* Hide overlay when esc key is pressed */
 $("body").keydown(function(event) {
     if (event.which == 27) {
-    $overlay.fadeOut(1000).hide();
+        $overlay.fadeOut(1000).hide();
     }
 });
 
@@ -99,6 +99,10 @@ function updateImage(imageLocation, imageCaption) {
 function nextImage() {
      /* update index */
     $index++;
+    /* loop up to first image in gallery */
+    if ($index >= $("#gallery li").length) {
+        $index = 0;
+    }
     /* use index to get next image */
     var nextImage = $("#gallery li").get($index).getElementsByTagName("a");
     /* get new image location and caption */
@@ -111,6 +115,10 @@ function nextImage() {
 function previousImage() {
     /* update the index */
     $index--;
+    /* loop back to last image in gallery */
+    if ($index < 0) {
+        $index = $("#gallery li").length - 1;
+    }
     /* get the previous image by index */
     var prevImage = $("#gallery li").get($index).getElementsByTagName("a");
     /* update the image location and caption */
